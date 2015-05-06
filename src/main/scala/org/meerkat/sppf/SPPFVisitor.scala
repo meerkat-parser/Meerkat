43cdc582695c80fe0e59b9a3c7b459e0ac467d13
Ali Afroozeh <afroozeh@gmail.com>
2015-05-06 18:21:02 +0200

    Working on SPPF

--- src/main/scala/org/meerkat/sppf/SPPFVisitor.scala
+++ src/main/scala/org/meerkat/sppf/SPPFVisitor.scala
@@ -1,9 +1,20 @@
 package org.meerkat.sppf
 
+import org.meerkat.tree.TreeVisitor
+import org.meerkat.meerkat.Rule
+import org.meerkat.tree.Tree
+
 object SPPFVisitor {
   
   def preOrder(node: SPPFNode)(f: SPPFNode => Unit): Unit = { f(node); node.children.foreach {x => preOrder(x)(f)} }
 
   def postOrder(node: SPPFNode)(f: SPPFNode => Unit): Unit = { node.children.foreach {x => postOrder(x)(f)}; f(node) }
-
+  
+  def build(n: NonterminalNode, visitor: TreeVisitor): Tree = {
+    
+    
+    
+    ???
+  }
+  
 }
