04b4ad68f89b414c5e231c6daeb04a11a1816f08
Anastassija <A.Izmaylova@cwi.nl>
2015-05-08 00:50:57 +0200

    Sketching a new design for Meerkats.

--- src/main/scala/org/meerkat/tmp/AbstractParsers.scala
+++ src/main/scala/org/meerkat/tmp/AbstractParsers.scala
@@ -3,10 +3,14 @@ package org.meerkat.tmp
 import org.meerkat.sppf.SPPFLookup
 import org.meerkat.util.Input
 
-trait MonadPlus[+T, M[_] <: MonadPlus[_,M]] {
-  def map[U](f: T => U): M[U]
-  def flatMap[U](f: T => M[U]): M[U]
+trait MonadPlus[+T, M[+F] <: MonadPlus[F,M]] {
+  def map[U](f: T => U)(implicit m: Memoizable[T]): M[U]
+  def flatMap[U](f: T => M[U])(implicit m: Memoizable[T]): M[U]
   def orElse[U >: T](r: => M[U]): M[U]
+  def filter(pred: T => Boolean): M[T]
+  
+  // Optimization
+  def _map[U](f: T => U): M[U]
 }
 
 trait AbstractParsers {
@@ -27,18 +31,21 @@ trait AbstractParsers {
     protected def isTerminal = false
   }
   
-  trait Composable[A, B] {
+  trait Composable[A, B] {    
+    type R
     
-    type R  
     type Sequence <: AbstractParser[R]
-    type Alternation <: AbstractParser[B]
+    def sequence(f: (Input, Int, SPPFLookup) => Result[R]): Sequence
     
     def index(a: A): Int
-    def intermediate(a: A, b: B, p: AbstractParser[R]): R
-    def nonterminal(elem: B, p: AbstractParser[B], nt: AbstractParser[Any]): B
+    def intermediate(a: A, b: B, p: AbstractParser[R]): R    
+  }
+  
+  trait Alternative[A, B >: A] {
+    type Alternation <: AbstractParser[B]
+    def alternation(f: (Input, Int, SPPFLookup) => Result[B]): Alternation
     
-    def sequence(f: (Input, Int, SPPFLookup) => Result[R]): Sequence
-    def alternation(f: (Input, Int, SPPFLookup) => Result[B]): Alternation    
+    def result(elem: B, p: AbstractParser[B], nt: AbstractParser[Any]): B
   }
   
   object AbstractParser {
@@ -46,18 +53,18 @@ trait AbstractParsers {
     def parser[T](p: Parser[T]): AbstractParser[T] 
       = new AbstractParser[T] { def apply(input: Input, i: Int, sppfLookup: SPPFLookup) = p(input, i, sppfLookup) } 
     
-    protected def seq[A, B](p1: AbstractParser[A], p2: AbstractParser[B])(implicit builder: Composable[A, B]): builder.Sequence = {
+    protected def seq[A: Memoizable, B: Memoizable](p1: AbstractParser[A], p2: AbstractParser[B])(implicit builder: Composable[A, B]): builder.Sequence = {
       lazy val q: builder.Sequence = builder sequence { 
-        (input, i, sppfLookup) => p1(input, i, sppfLookup) flatMap { x1 => p2(input, builder index x1, sppfLookup).map { x2 => builder intermediate (x1, x2, q) } } 
+        (input, i, sppfLookup) => p1(input, i, sppfLookup) flatMap { x1 => p2(input, builder index x1, sppfLookup)._map { x2 => builder intermediate (x1, x2, q) } } 
       }
       q
     }
   
-    protected def alt[A, B >: A](p1: AbstractParser[A], p2: AbstractParser[B])(implicit builder: Composable[A, B]): builder.Alternation = {
+    protected def alt[A, B >: A](p1: AbstractParser[A], p2: AbstractParser[B])(implicit builder: Alternative[A, B], m1: Memoizable[A], m2: Memoizable[B]): builder.Alternation = {
       lazy val q: builder.Alternation = builder alternation { 
         lazy val q1: AbstractParser[A] = if (p1 isNonterminal) parser(p1) else p1 passHead q.head
         lazy val q2: AbstractParser[B] = if (p2 isNonterminal) parser(p2) else p2
-        (input, i, sppfLookup) => p1(input, i, sppfLookup).map(x1 => builder nonterminal (x1, q1, q.head)) orElse p2(input, i, sppfLookup).map(x2 => builder nonterminal (x2, q2, q.head)) 
+        (input, i, sppfLookup) => p1(input, i, sppfLookup).map(x1 => builder result (x1, q1, q.head)) orElse p2(input, i, sppfLookup).map(x2 => builder result (x2, q2, q.head)) 
       }
       q
     } 
---
+++ src/main/scala/org/meerkat/tmp/CPSResult.scala
@@ -0,0 +1,61 @@
+package org.meerkat.tmp
+
+import java.util.Deque
+import java.util.ArrayDeque
+
+trait Memoizable[-T] { 
+  type U 
+  def value(t: T): U 
+}
+
+trait CPSResult[+T] extends ((T => Unit) => Unit) with MonadPlus[T, CPSResult] { import CPSResult._
+  
+  def map[U](f: T => U)(implicit m: Memoizable[T]) = result[U](k => this(memo_k(k compose f)))
+  
+  def flatMap[U](f: T => CPSResult[U])(implicit m: Memoizable[T]) = result[U](k => this(memo_k(t => f(t)(k))))
+  
+  def orElse[U >: T](r: => CPSResult[U]) = result[U](k => Trampoline.alt(this, k, r))
+  
+  def filter(pred: T => Boolean) = result[T](k => this(t => if(pred(t)) k(t)))
+  
+  // Optimization
+  def _map[U](f: T => U) = result[U](k => this(k compose f))
+}
+
+object CPSResult {
+  
+  type K[T] = T => Unit
+  
+  def result[T](f: K[T] => Unit): CPSResult[T] = new CPSResult[T] { def apply(k: K[T]) = f(k) }
+  def success[T](t: T): CPSResult[T] = new CPSResult[T] { def apply(k: K[T]) = k(t) }
+  def failure[T]: CPSResult[T] = new CPSResult[T] { def apply(k: K[T]) = () }
+  
+  def memo[T](res: => CPSResult[T]): CPSResult[T] = { import java.util.Set; import java.util.LinkedHashSet
+    val Ks: Deque[K[T]] = new ArrayDeque[K[T]]()
+    val Rs: Set[T] = new LinkedHashSet[T]()
+    
+    new CPSResult[T] { 
+      def apply(k: K[T]) = { 
+        if(Ks.isEmpty) {
+          Ks.push(k)
+          res((t: T) => { 
+                if(!Rs.contains(t)) {
+                  Rs.add(t)
+                  val it = Ks.iterator()
+                  while(it hasNext) Trampoline.call(it.next(), t) 
+                }
+              })
+        } else { 
+          Ks.push(k) 
+          val it = Rs.iterator()
+          while(it hasNext) Trampoline.call(k, it.next())
+        } 
+      } 
+    }
+  }
+  
+  protected def memo_k[T](f: T => Unit)(implicit m: Memoizable[T]): T => Unit = {
+    val s: java.util.Set[m.U] = new java.util.HashSet[m.U]()
+    t => if(!s.contains(m.value(t))) { s.add(m.value(t)); f(t)}
+  }
+}
