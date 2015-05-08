84d1eda406c88ac1cfca83c357cec68557967edd
Anastassija <A.Izmaylova@cwi.nl>
2015-05-08 23:15:29 +0200

    Working on operator precedence for Meerkats.

--- src/main/scala/org/meerkat/tmp/OperatorParsers.scala
+++ src/main/scala/org/meerkat/tmp/OperatorParsers.scala
@@ -10,34 +10,33 @@ object OperatorParsers {
   
   trait Sequence extends AbstractOperatorParser[NonPackedNode]
   
-  case class Postfix(p1: AbstractOperatorParser[NonPackedNode], p2: Parsers.Symbol) extends Sequence {
-    def apply(prec: Prec) = ???
+  case class Postfix(p1: AbstractOperatorParser[NonPackedNode], p2: AbstractCPSParsers.AbstractParser[NonPackedNode]) extends Sequence { import Parsers._
     
-    def ~ (p: Parsers.Symbol): Sequence = Postfix(this, p)
-    def ~ (p: Nonterminal): Sequence = Infix(this, p)
+    def apply(prec: Prec): Parsers.Sequence = AbstractCPSParsers.AbstractParser.seq(p1(prec), p2)
+    
+    def ~ (p: Parsers.Symbol): OperatorParsers.Sequence = Postfix(this, p)
+    def ~ (p: OperatorParsers.Nonterminal): OperatorParsers.Sequence = Infix(this, p)
   }
   
-  case class Prefix(p1: Sequence, p2: Nonterminal) extends Sequence {
-    def apply(prec: Prec) = ???
+  case class Prefix(p1: Parsers.Sequence, p2: Nonterminal) extends Sequence { import Parsers._
     
-    def ~ (p: Parsers.Symbol): Parsers.Sequence 
-      = new Parsers.Sequence { import Parsers._
-         val f = AbstractCPSParsers.AbstractParser.seq(Prefix.this($), p)
-         def apply(input: Input, i: Int, sppfLookup: SPPFLookup) = f(input, i, sppfLookup)
-        }
-    def ~ (p: Nonterminal): Sequence = ???
+    def apply(prec: Prec): Parsers.Sequence = AbstractCPSParsers.AbstractParser.seq(p1, p2(prec))
+    
+    def ~ (p: Parsers.Symbol): Parsers.Sequence = AbstractCPSParsers.AbstractParser.seq(Prefix.this($), p)
+    def ~ (p: OperatorParsers.Nonterminal): OperatorParsers.Sequence = Prefix(Prefix.this($), p)
   }
   
-  case class Infix(p1: Postfix, p2: Nonterminal) extends Sequence {
-    def apply(prec: Prec) = ???
+  case class Infix(p1: AbstractOperatorParser[NonPackedNode], p2: Nonterminal) extends Sequence { import Parsers._
     
-    def ~ (p: Parsers.Symbol): Sequence = ???
-    def ~ (p: Nonterminal): Sequence = ???
+    def apply(prec: Prec): Parsers.Sequence = AbstractCPSParsers.AbstractParser.seq(p1(prec), p2(prec))
+    
+    def ~ (p: Parsers.Symbol): OperatorParsers.Sequence = Postfix(Postfix(p1, p2($)), p)
+    def ~ (p: OperatorParsers.Nonterminal): OperatorParsers.Sequence = Infix(Postfix(p1, p2($)), p)
   }
   
   trait Nonterminal extends AbstractOperatorParser[NonPackedNode] {
-    def ~ (p: Parsers.Symbol): Sequence = ???
-    def ~ (p: Nonterminal): Sequence = ???
+    def ~ (p: Parsers.Symbol): Sequence = Postfix(this, p)
+    def ~ (p: Nonterminal): Sequence = Infix(this, p)
   }
   
 
