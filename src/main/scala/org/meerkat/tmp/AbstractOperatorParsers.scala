fe78c2bd0f74eeb5149c5e82e902adcf272372e3
Anastassija <A.Izmaylova@cwi.nl>
2015-05-08 18:19:16 +0200

    Working on operator precedence for Meerkats.

---
+++ src/main/scala/org/meerkat/tmp/AbstractOperatorParsers.scala
@@ -0,0 +1,33 @@
+package org.meerkat.tmp
+
+object Rec extends Enumeration {
+  type Rec = Value
+  val UNKNOWN, NON, LEFT, RIGHT, BOTH = Value
+}
+
+object Group extends Enumeration {
+  type Group = Value
+  val UNKNOWN, NON, LEFT, RIGHT, ASSOC, NON_ASSOC = Value
+}
+
+object AbstractOperatorParsers {
+  
+  import AbstractCPSParsers._
+  
+  type Pred = (Int, Int)
+  
+  trait AbstractOperatorParser[T] extends (Pred => AbstractParser[T]) {
+     
+    private var rec: Rec.Rec = Rec.UNKNOWN
+    private var group: Group.Group = Group.UNKNOWN
+    
+    def defineRecursion(rec: Rec.Rec): Unit = this.rec = rec
+    def defineGroup(group: Group.Group): Unit = this.group = group
+     
+    def isLeft = rec == Rec.LEFT || rec == Rec.BOTH
+    def isRight = rec == Rec.RIGHT || rec == Rec.BOTH
+    def isLeftOrRight = Rec.BOTH
+    
+  }
+
+}
